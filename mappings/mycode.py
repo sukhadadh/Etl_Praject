2623af05caece487774468525dbf3d2b3096c2bf
sukhada <sukhadad95@gmail.com>
2026-05-28 16:34:03 +0530

    code changed

--- mappings/mycode.py
+++ mappings/mycode.py
@@ -11,4 +11,15 @@ df = spark.read.csv('/samples/sales.csv', sep =',' , inferSchema = True, header 
 #show data
 df.show(5)
 
+#count total rows
+count = df.count()
+print("total number of rows", count)
 
+#reading specific columns(select)
+df1 = df.select("sale_id" , "sale_date")
+df1.show()
+
+#filter (where)
+filtered_df = df.filter('total_amount > 50.00')
+filtered_df.show()
+print(filtered_df.count())
